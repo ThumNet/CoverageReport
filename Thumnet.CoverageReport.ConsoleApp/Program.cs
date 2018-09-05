75f108e98bec13807cd902b895b2116c2a74a9d2
Jeffrey Tummers <jeffreytummers@gmail.com>
2018-09-05 22:19:33 +0200

    Add lz-string compression and some styling

--- Thumnet.CoverageReport.ConsoleApp/Program.cs
+++ Thumnet.CoverageReport.ConsoleApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LZString;
+using System;
 using System.IO;
 using System.Linq;
 using Thumnet.CoverageReport.Core;
@@ -18,7 +19,16 @@ namespace Thumnet.CoverageReport.ConsoleApp
             var items = parser.ReadItems().ToList();
             var sourceFiles = items
                 .Select(i => i.File)
-                .ToDictionary(k => k, v => File.ReadAllText(v));
+                .ToDictionary(k => k, v => LzString.CompressToUtf16(File.ReadAllText(v)));
+
+            //var source = File.ReadAllText(items[0].File);
+            //Console.WriteLine(source);
+            //var compressed = LzString.CompressToUtf16(source);
+            //Console.WriteLine();
+            //Console.WriteLine(compressed);
+            //var decompressed = LzString.DecompressFromUtf16(compressed);
+
+            //Console.ReadLine();
 
             var inlineTemplate = new HtmlInlineTemplate(string.Join(Environment.NewLine, parser.TextLines), sourceFiles);
             var generated = inlineTemplate.TransformText();
