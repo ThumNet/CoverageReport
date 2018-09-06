0e6ffc17cc68740ecdc3d12c67c4c3264cc170dd
Jeffrey Tummers <jeffreytummers@gmail.com>
2018-09-07 01:28:24 +0200

    More styling plus starten with hierarchy view

--- Thumnet.CoverageReport.Core/Generators/scripts/tools.js
+++ Thumnet.CoverageReport.Core/Generators/scripts/tools.js
@@ -4,7 +4,7 @@
     var str = longest = '';
     for (var i = 0; i < parts.length; i++) {
         str += parts[i] + separator;
-        if (_.every(sorted, function (s) { return s.indexOf(str) === 0 })) { // IE11 doesn't support startsWith :(
+        if (_.every(sorted, function (s) { return s.indexOf(str) === 0; })) { // IE11 doesn't support startsWith :(
             longest = str;
         } else {
             break;
@@ -25,4 +25,18 @@ function parseHashBangArgs(aURL) {
         vars[hash[0]] = hash.length > 1 ? decodeURI(hash[1]) : null;
     }
     return _.size(vars) > 0 ? vars : null;
+}
+
+function hierarchy(paths, seperator = '/') {
+    return paths.reduce(function (hier, path) {
+        var x = hier;
+        path.split(seperator).forEach(function (item) {
+            if (!x[item]) {
+                x[item] = { };
+            }
+            x = x[item];
+        });
+        x.path = path;
+        return hier;
+    }, { });
 }
